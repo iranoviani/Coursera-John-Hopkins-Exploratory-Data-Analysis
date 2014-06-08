7f17061e8ed3ecf3d6bb400d1ccd763b5eb48791
Ira Noviani <iranoviani@yahoo.com>
2014-06-08 06:57:31 +0100

    Plot 1 Upload
    Uploaded both the image and the R file

---
+++ plot1.R
@@ -0,0 +1,17 @@
+##Get and load the data
+data <- subset(read.table("household_power_consumption.txt", 
+                          header = TRUE, 
+                          sep=";", 
+                          na.strings = "?"), 
+               as.Date(Date, "%d/%m/%Y") >= "2007-02-01" & 
+                 as.Date(Date, "%d/%m/%Y") <= "2007-02-02")
+
+##Create histogram
+hist(data$Global_active_power, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power")
+
+##Copy to graphic devices
+dev.copy(png, file = "plo1.png", width = 480, height = 480, units = "px")
+dev.off()
